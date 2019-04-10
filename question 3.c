ad3b6ccf1c895a53856c128dac40620b07ef0953
jaypathak14 <36795128+jaypathak14@users.noreply.github.com>
2019-04-10 20:28:42 +0530

    added comments

--- question 3.c
+++ question 3.c
@@ -5,17 +5,29 @@ int main()
 {
   int time,bt[10],at[10],sum_bt=0,smallest,rem,n,i,rt[10];//declaring the varialbe
   int sumt=0,sumw=0;//declaring the varialbe
-  printf("no of processes : ");//will ask the no. of processes
-  scanf("%d",&n);rem=n;//take the input of processes
+  printf("\nno of processes : ");//will ask the no. of processes
+  scanf("%d",&n);//take the input of processes
+  rem=n;
   for(i=0;i<n;i++)//ask the user to enter the details for each details
   {
-    printf("arrival time for process P%d : ",i+1);//ask to enter arrival time
-    scanf("%d",&at[i]);//take the input for arrival time
-    printf("burst time for process P%d : ",i+1);//ask to enter burst time
-    scanf("%d",&bt[i]);rt[i]=bt[i];//take the input for arrival time
+   		printf("\nArrival time for process P%d : ",i+1);//ask to enter arrival time
+   		scanf("%d",&at[i]);//take the input for arrival time
+    	printf("\nBurst time for process P%d : ",i+1);//ask to enter burst time
+    	scanf("%d",&bt[i]);//take the input for arrival time
+		rt[i]=bt[i];
    
   }
-  rt[9]=bt[9]=9999;               
+  for(i=0;i<n;i++)//checking the non zero condition of program
+  {
+    if(at[i]==0)
+  	{
+  		printf("\nerror"); 	
+  		printf("\narival time cannot be 0");
+  		exit(0);//exit the process if there is any process starting with 0
+  }
+  
+  
+  rt[9]=bt[9]=9999; //giving the opposite condition as highest        
   for(time=0;rem!=0;time++)
   {
     smallest=9;
@@ -29,12 +41,12 @@ if(rt[smallest]==0)
 {
 rem--;
     printf("P[%d]\t|\t%d\t|\t%d\n",smallest+1,time+1-bt[smallest]-at[smallest],time+1-at[smallest]);
-    sumt+=time+1-bt[smallest]-at[smallest];
+    sumt+=time+1-bt[smallest]-at[smallest]; 
     sumw+=time+1-at[smallest];
     
    
   }}
-  printf("\n\n average turnaround time = %f",sumw*1.0/n);
+  printf("\n\n average turnaround time = %f",sumw*1.0/n);//integer convertion to float and giving value in decimals
   printf("\n\n average waiting time = %f",sumt*1.0/n);
   return 0;
-}
+}}
